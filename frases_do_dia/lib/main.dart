458c3dbbb7522d6b7ef11e30c9fc0821e904ed8f
Wictor Chaves <wictor@hotmail.com.br>
2020-04-11 23:52:39 -0300

    Text Align

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -13,7 +13,10 @@ void main(){
       ),
       child: Column(
         children: [
-          Text("Texto"),
+          Text(
+              "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+              textAlign: TextAlign.justify,
+          ),
         ],
       ),
     ),
