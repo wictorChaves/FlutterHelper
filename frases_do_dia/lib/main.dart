92af3b0eba9a41f54aae5256910c9c02dea7c8ae
Wictor Chaves <wictor@hotmail.com.br>
2020-04-11 23:16:56 -0300

    Colunas

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     title: "Frases do dia",
-    home: Container(
-      color: Colors.white,
-    ),
+    home: Column(children: <Widget>[
+      Text("Usuário:"),
+      Text("Senha:")
+    ],),
   ));
 }
