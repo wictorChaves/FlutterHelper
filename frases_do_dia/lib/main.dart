d9fb66335277cb2d33097bc733ce349a53178261
Wictor Chaves <wictor@hotmail.com.br>
2020-04-11 23:50:35 -0300

    Border.

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
     title: "Frases do dia",
     home: Container(
-      color: Colors.white,
+      //color: Colors.white,
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 3,
+          color: Colors.red
+        )
+      ),
       child: Column(
         children: [
           Text("Texto"),
