68d914bca339cff3111442ffe5fa146a0eb0062d
Wictor Chaves <wictor@hotmail.com.br>
2020-04-12 00:51:24 -0300

    Stateless

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -3,13 +3,20 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("Frases do dia"),
         backgroundColor: Colors.green,
       ),
       body: Text("Conteúdo"),
       bottomNavigationBar: BottomAppBar(),
-    ),
-  ));
+    );
+  }
 }
