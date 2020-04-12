01a3b0d04ad3a61aaca8660b098b061991c73aa0
Wictor Chaves <wictor@hotmail.com.br>
2020-04-11 23:48:22 -0300

    Remover faixa de debug.

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -2,22 +2,13 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: "Frases do dia",
     home: Container(
       color: Colors.white,
       child: Column(
         children: [
-          FlatButton(
-            onPressed: null,
-            child: Text(
-              "Botão",
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.black,
-                decoration: TextDecoration.none
-              ),
-            ),
-          ),
+          Text("Texto"),
         ],
       ),
     ),
