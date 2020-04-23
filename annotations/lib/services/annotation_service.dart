ec5b4a66ea4ce18c3fa907dac6b1b99ac39186d5
Wictor Chaves <wictor@hotmail.com.br>
2020-04-23 19:28:31 -0300

    Transformando a classe AnnotationService em uma classe singleton.

--- annotations/lib/services/annotation_service.dart
+++ annotations/lib/services/annotation_service.dart
@@ -1,5 +1,11 @@
 import 'package:annotations/services/main_service.dart';
 
 class AnnotationService extends MainService {
-  AnnotationService() : super("annotation") {}
+
+  static final AnnotationService _singleton = AnnotationService._internal();
+  AnnotationService._internal() : super("annotation");
+  factory AnnotationService(){
+    return _singleton;
+  }
+
 }
