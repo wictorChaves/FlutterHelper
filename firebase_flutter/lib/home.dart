eb9f152eb8c8b9dd384cb5176863cadc99df3f1e
Wictor Chaves <wictor@hotmail.com.br>
2020-04-30 00:00:28 -0300

    Upload da imagem para o Firestorage.

--- firebase_flutter/lib/home.dart
+++ firebase_flutter/lib/home.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:image_picker/image_picker.dart';
@@ -26,15 +27,23 @@ class _HomeState extends State<Home> {
     setState(() => _image = image);
   }
 
+  _uploadImage() async {
+    FirebaseStorage storage = FirebaseStorage.instance;
+    StorageReference rootPath = storage.ref();
+    StorageReference file = rootPath.child("images").child("foto1.png");
+    file.putFile(_image);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text(APP_TITLE)),
         body: Container(
             padding: EdgeInsets.all(20),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
+            child: SingleChildScrollView(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
                   RaisedButton.icon(
                     onPressed: _getCameraImage,
                     icon: Icon(Icons.camera_alt),
@@ -45,7 +54,14 @@ class _HomeState extends State<Home> {
                     icon: Icon(Icons.image),
                     label: Text("Galeria"),
                   ),
-                  _image == null ? Container() : Image.file(_image)
-                ])));
+                  _image == null ? Container() : Image.file(_image),
+                  _image == null
+                      ? Container()
+                      : RaisedButton.icon(
+                          onPressed: _uploadImage,
+                          icon: Icon(Icons.cloud_upload),
+                          label: Text("Upload Imagem"),
+                        ),
+                ]))));
   }
 }
