598e4debe8bcb9d77fa696994a39e531ad22c3e1
Wictor Chaves <wictor@hotmail.com.br>
2020-05-16 22:08:13 -0300

    commit do fim do dia.

---
+++ firebase_flutter/whatsapp/maps_geolocation/my_travels/uber/lib/configs/main_config.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const APP_TITLE = "Uber";
+const PRIMARY_COLOR = Colors.blue;
---
+++ firebase_flutter/whatsapp/maps_geolocation/my_travels/uber/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:uber/screens/home.dart';
+
+import 'configs/main_config.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: APP_TITLE,
+      theme: ThemeData(
+          primarySwatch: PRIMARY_COLOR,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: Home()));
+}
